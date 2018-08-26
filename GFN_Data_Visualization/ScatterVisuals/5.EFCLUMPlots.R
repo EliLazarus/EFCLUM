caa30a0166402b6338afb6b069b059639d006bdc
EliLazarus <lazarus.eli@gmail.com>
2018-08-26 12:27:35 -0700

    textGrob height as inputs in plot funtion

--- GFN_Data_Visualization/ScatterVisuals/5.EFCLUMPlots.R
+++ GFN_Data_Visualization/ScatterVisuals/5.EFCLUMPlots.R
@@ -51,40 +51,37 @@ if (file.exists("./GFN_Data_Visualization/ScatterVisuals/Plots/")){
 }
 
 ##TEST PLOT for Plot Function
-myColors <- #brewer.pal(8,"Set1")
-  c("yellow","darkorange2","black", "purple4", "forestgreen", "grey30", "blue", "grey70")
-names(myColors) <- levels(EFWBData11.Services$Continents)
-colScale <- scale_colour_manual(name = "Continents",values = myColors)
-#
-p <- ggplot(EFWBData11.Services, aes(ZScore_Index, total,colour=Continents)) + geom_point(size = 5) +
-  geom_text(label = EFWBData11.Services$GTAP_name,hjust=-.1, colour="grey") +
-  colScale +
-  theme(plot.margin = unit(c(1,4,1,1),"cm")) +
-  # annotation_custom(textGrob(subset(IndicatorsDownloaded[,2],IndicatorsDownloaded$CLUM=="Services"))) +
-  # xmin=-.5,xmax=1,ymin=-2,ymax=-1) +
-  #
-  labs(title = paste("World Bank Data, CLUM category:",EFWBData11.Services$clum7_name),
-       y = "EF per capita"); p
-# annotate("text", x = 0, y = -1:-17, hjust = 0,
-#          label = subset(IndicatorsDownloaded[,2],IndicatorsDownloaded$CLUM=="Services"))
-# labs(caption = "text",subset(IndicatorsDownloaded[,2],IndicatorsDownloaded$CLUM=="Services"))
-gt <- ggplot_gtable(ggplot_build(p))
-gt$layout$clip[gt$layout$name == "panel"] <- "off"
-tb <- tableGrob(subset(SDGIndicatorsDownloaded[,2],SDGIndicatorsDownloaded$CLUM=="Services"),
-                theme = ttheme_default(base_size=9, margin = theme(plot.margin = unit(c(2,2,2,2), "cm")),core = list(fg_params=list(hjust=0, x=0))))
-#rowhead = list(fg_params=list(hjust=0, x=0))))
-grid.arrange(gt, tb, nrow=2, heights = c(2,1))
+# myColors <- #brewer.pal(8,"Set1")
+#   c("yellow","darkorange2","black", "purple4", "forestgreen", "grey30", "blue", "grey70")
+# names(myColors) <- levels(EFWBData11.Services$Continents)
+# colScale <- scale_colour_manual(name = "Continents",values = myColors)
+# #
+# p <- ggplot(EFWBData11.Services, aes(ZScore_Index, total,colour=Continents)) + geom_point(size = 5) +
+#   geom_text(label = EFWBData11.Services$GTAP_name,hjust=-.1, colour="grey") +
+#   colScale +
+#   theme(plot.margin = unit(c(1,4,1,1),"cm")) +
+#   # annotation_custom(textGrob(subset(IndicatorsDownloaded[,2],IndicatorsDownloaded$CLUM=="Services"))) +
+#   # xmin=-.5,xmax=1,ymin=-2,ymax=-1) +
+#   #
+#   labs(title = paste("World Bank Data, CLUM category:",EFWBData11.Services$clum7_name),
+#        y = "EF per capita"); p
+# # annotate("text", x = 0, y = -1:-17, hjust = 0,
+# #          label = subset(IndicatorsDownloaded[,2],IndicatorsDownloaded$CLUM=="Services"))
+# # labs(caption = "text",subset(IndicatorsDownloaded[,2],IndicatorsDownloaded$CLUM=="Services"))
+# gt <- ggplot_gtable(ggplot_build(p))
+# gt$layout$clip[gt$layout$name == "panel"] <- "off"
+# tb <- tableGrob(subset(SDGIndicatorsDownloaded[,2],SDGIndicatorsDownloaded$CLUM=="Services"),
+#                 theme = ttheme_default(base_size=9, margin = theme(plot.margin = unit(c(2,2,2,2), "cm")),core = list(fg_params=list(hjust=0, x=0))))
+# #rowhead = list(fg_params=list(hjust=0, x=0))))
+# grid.arrange(gt, tb, nrow=2, heights = c(2,1))
+# 
+# #plotname <- paste(substr(EFWBData11.Services,1,5),substr(EFWBData11.Services,13,17),".pdf")
+# #dev.off()
+# #grid.draw(gt)
+# #subset(IndicatorsDownloaded[,2],IndicatorsDownloaded$CLUM=="Services")
 
-plotname <- paste(substr(EFWBData11.Services,1,5),substr(EFWBData11.Services,13,17),".pdf")
-pdf(Test.pdf,width=6,height=4)
-dev.off()
-
-#grid.draw(gt)
-#subset(IndicatorsDownloaded[,2],IndicatorsDownloaded$CLUM=="Services")
-
-plotfunc <- function(data, title, CLUMcat, IndDL){
-  myColors <-   c("yellow","darkorange2","black", "purple3", "forestgreen", "grey30", "royalblue", "grey70")
-#brewer.pal(8,"Set1")
+plotfunc <- function(data, title, CLUMcat, IndDL, height1, height2){
+  myColors <-   c("gold","black", "forestgreen", "royalblue", "darkorange2", "grey30", "grey70", "purple3")
   names(myColors) <- levels(data$Continents)
   colScale <- scale_colour_manual(name = "Continents",values = myColors)
   p <- ggplot(data, aes(ZScore_Index, total , colour = Continents)) +
@@ -105,7 +102,7 @@ plotname <- paste("./GFN_Data_Visualization/ScatterVisuals/Plots/",deparse(subst
 pdf(plotname,width=12,height=9,onefile = FALSE)
 dev.off
 grid.newpage()
-grid.arrange(gt, tb, nrow=2, heights = c(1.5,.5))
+grid.arrange(gt, tb, nrow=2, heights = c(height1, height2))
 dev.off()
 #, main=textGrob("Total Data and Image", gp=gpar(cex=3)), ncol = 2,
            #  widths=unit.c(grobWidth(gt), unit(1,"npc") - grobWidth(tb)))
@@ -126,31 +123,33 @@ pause <- function (secs){
 # pdf("Test.pdf", onefile = FALSE,width=12,height=9)#, paper = "special")
 # dev.off()
 
+#Plot and output all SDG Indicators 
+plotfunc(EFSDGData11.Food,"SDG Data", "Food", SDGIndicatorsDownloaded, 1.5, .5)
+pause(waitlength)
+plotfunc(EFSDGData11.Goods,"SDG Data", "Goods",SDGIndicatorsDownloaded, 1.5, .5)
+pause(waitlength)
+plotfunc(EFSDGData11.Housing,"SDG Data", "Housing", SDGIndicatorsDownloaded, 1.5, .5)
+pause(waitlength)
+plotfunc(EFSDGData11.Services,"SDG Data", "Services", SDGIndicatorsDownloaded, 1.5, .5)
+pause(waitlength)
+plotfunc(EFSDGData11.PersonalTransportation,"SDG Data", "Personal Transportation", SDGIndicatorsDownloaded, 1.5, .5)
+pause(waitlength)
+plotfunc(EFSDGData11.Government,"SDG Data", "Government", SDGIndicatorsDownloaded, 1.5, .5)
+pause(waitlength)
+plotfunc(EFSDGData11.GrossFixedCapitalFormation,"SDG Data", "Gross Fixed Capital Formation", SDGIndicatorsDownloaded, 1.5, .5)
+pause(waitlength)
 
+#Plot and output all WB Indicators
+plotfunc(EFWBData11.Food,"World Bank Data", "Food",IndicatorsDownloaded, 1.5, .5)
 pause(waitlength)
-plotfunc(EFSDGData11.Goods,"SDG Data", "Goods",SDGIndicatorsDownloaded)
+#Added manual line to IndicatorsDownloaded for WBGoods to work with function
+plotfunc(EFWBData11.Goods,"World Bank Data", "Goods", IndicatorsDownloaded, 1.5, .5)
 pause(waitlength)
-plotfunc(EFSDGData11.Government,"SDG Data", "Government", SDGIndicatorsDownloaded)
+plotfunc(EFWBData11.Services,"World Bank Data", "Services",IndicatorsDownloaded, 1, .8)
 pause(waitlength)
-plotfunc(EFSDGData11.Housing,"SDG Data", "Housing", SDGIndicatorsDownloaded)
+plotfunc(EFWBData11.Housing,"World Bank Data", "Housing",IndicatorsDownloaded, 1.5, .5)
 pause(waitlength)
-plotfunc(EFSDGData11.GrossFixedCapitalFormation,"SDG Data", "Gross Fixed Capital Formation", SDGIndicatorsDownloaded)
+plotfunc(EFWBData11.PersonalTransportation,"World Bank Data", "Personal Transportation",IndicatorsDownloaded, 1.5, .5)
 pause(waitlength)
-plotfunc(EFSDGData11.PersonalTransportation,"SDG Data", "Personal Transportation", SDGIndicatorsDownloaded)
+plotfunc(EFWBData11.Government,"World Bank Data", "Government",IndicatorsDownloaded, 1, 1)
 pause(waitlength)
-plotfunc(EFSDGData11.Food,"SDG Data", "Food", SDGIndicatorsDownloaded)
-pause(waitlength)
-
-
-plotfunc(EFWBData11.PersonalTransportation,"World Bank Data", "Personal Transportation",IndicatorsDownloaded)
-pause(waitlength)
-plotfunc(EFWBData11.Housing,"World Bank Data", "Housing",IndicatorsDownloaded)
-pause(waitlength)
-plotfunc(EFWBData11.Government,"World Bank Data", "Government",IndicatorsDownloaded)
-pause(waitlength)
-plotfunc(EFWBData11.Food,"World Bank Data", "Food",IndicatorsDownloaded)
-pause(waitlength)
-plotfunc(EFWBData11.Services,"World Bank Data", "Services",IndicatorsDownloaded)
-
-#Goods DOES NOT HAVE WB Data!!!!
-plotfunc(EFWBData11.Goods,"World Bank Data", "Goods", IndicatorsDownloaded)
